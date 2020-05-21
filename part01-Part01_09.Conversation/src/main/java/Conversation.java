f26b95b8331ad916a8478ffe77d99243dc96964d
Jereaaa <jere_calvet@hotmail.com>
2020-05-21 18:08:13 -0300

    Ejercicio

--- part01-Part01_09.Conversation/src/main/java/Conversation.java
+++ part01-Part01_09.Conversation/src/main/java/Conversation.java
@@ -6,7 +6,11 @@ public class Conversation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
-
+        System.out.println("Greetings! How are you doing?");
+        scanner.nextLine();
+        System.out.println("Oh, how interesting. Tell me more!");
+        scanner.nextLine();
+        System.out.println("Thanks for sharing!");
+        
     }
 }
