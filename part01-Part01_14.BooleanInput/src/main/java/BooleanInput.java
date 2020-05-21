7e5221efe32bcdec9d72516db72b443ca7fa2622
Jereaaa <jere_calvet@hotmail.com>
2020-05-21 19:07:39 -0300

    Convertir StrToBoolean

--- part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
+++ part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
@@ -6,7 +6,9 @@ public class BooleanInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // write your program here
+        System.out.println("Write something:");
+        boolean flag = Boolean.valueOf(scanner.nextLine());
+        System.out.println("True or false? " + flag);
 
     }
 }
