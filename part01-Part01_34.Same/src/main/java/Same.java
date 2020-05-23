7646763e61711921f7ceaf452afeddf2a810b599
Jereaaa <jere_calvet@hotmail.com>
2020-05-23 18:19:22 -0300

    Condicionales: IF ELSE statement. Comparando strings: String.Equals()

--- part01-Part01_34.Same/src/main/java/Same.java
+++ part01-Part01_34.Same/src/main/java/Same.java
@@ -6,6 +6,14 @@ public class Same {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here. 
+        System.out.println("Enter the first string:"); 
+        String textA = scan.nextLine();
+        System.out.println("Enter the second string:"); 
+        String textB = scan.nextLine();
+        if (textA.equals(textB)) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Different");
+        }
     }
 }
