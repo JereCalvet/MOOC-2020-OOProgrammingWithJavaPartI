4d026b6cf59b04fd9157048756bf7ec71c2fed21
Jereaaa <jere_calvet@hotmail.com>
2020-05-23 17:58:15 -0300

    Condicionales: IF ELSE statement

--- part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
+++ part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -5,6 +5,16 @@ public class LargerThanOrEqualTo {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        System.out.println("Give the first number:");
+        int numA = Integer.valueOf(scan.nextLine());
+        System.out.println("Give the second number:");
+        int numB = Integer.valueOf(scan.nextLine());
+        if (numA > numB) {
+            System.out.println("Greater number is: " + numA);
+        } else if (numA < numB) {    
+            System.out.println("Greater number is: " + numB);
+        } else {
+            System.out.println("The numbers are equal!");
+        }
     }
 }
