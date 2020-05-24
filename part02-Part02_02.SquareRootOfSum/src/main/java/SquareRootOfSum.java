23b4ee6f1a3e1dbecfce49d50811a2f0908fd8cb
Jereaaa <jere_calvet@hotmail.com>
2020-05-24 17:29:30 -0300

    Ejercicio: calcular raiz cuadrada

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,8 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int numA = Integer.valueOf(scanner.nextLine());
+        int numB = Integer.valueOf(scanner.nextLine());
+        System.out.println(Math.sqrt(numA + numB));
     }
 }
