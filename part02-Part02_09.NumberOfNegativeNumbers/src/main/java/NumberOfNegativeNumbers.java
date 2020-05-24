b81111d55e7b0bfe36c9e1408972571fa6029821
Jereaaa <jere_calvet@hotmail.com>
2020-05-24 19:00:49 -0300

    Loop: int var como contador

--- part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
+++ part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -5,6 +5,18 @@ public class NumberOfNegativeNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int counter = 0;
 
+        while (true) {
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 0) {
+                break;
+            }
+            if (input < 0) {
+                counter++;
+            }
+        }
+        System.out.println("Number of negative numbers: " + counter);
     }
 }
--- part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -5,6 +5,15 @@ public class SumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int sum = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 0) {
+                break;
+            }
+            sum += input;
+        }
+        System.out.println("Sum of the numbers: " + sum);
     }
 }
