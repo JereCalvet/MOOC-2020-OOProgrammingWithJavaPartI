762b18c4a17235cee7903630a967ffdbc4e6d53f
Jereaaa <jere_calvet@hotmail.com>
2020-05-24 19:03:45 -0300

    Loop: int var como contador

--- part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -5,6 +5,18 @@ public class NumberAndSumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int counter = 0;
+        int sum = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 0) {
+                break;
+            }
+            sum += input;
+            counter++;
+        }
+        System.out.println("Number of numbers: " + counter);
+        System.out.println("Sum of the numbers: " + sum);
     }
 }
