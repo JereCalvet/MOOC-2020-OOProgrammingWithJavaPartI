da383b3e3ee0edd9b53375bfc557d3d8c9dd9655
Jereaaa <jere_calvet@hotmail.com>
2020-05-24 18:44:17 -0300

    Loop: int var como contador

--- part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,16 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int counter = 0;
 
+        while (true) {
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 0) {
+                break;
+            }
+            counter++;
+        }
+        System.out.println("Number of numbers: " + counter);
     }
 }
