184c67c1449cf330c2694ae74fac23b5d96318a0
Jereaaa <jere_calvet@hotmail.com>
2020-05-24 19:07:46 -0300

    Loop: int var como contador

--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,17 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int counter = 0;
+        int sum = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 0) {
+                break;
+            }
+            counter++;
+            sum += input;
+        }
+        System.out.println("Average of the numbers: " + (sum / (counter * 1.0)));
     }
 }
