7c0872953768d84b8e945047322b2cdfea3b67e9
Jereaaa <jere_calvet@hotmail.com>
2020-05-24 19:47:52 -0300

    Loop: int var como contador

--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,23 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int counter = 0;
+        int sum = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 0) {
+                break;
+            }
+            if (input > 0) {
+                sum += input;
+                counter++;
+            }
+        }
+        if (counter != 0) {
+            System.out.println(sum / (counter * 1.0));
+        } else {
+            System.out.println("Cannot calculate the average");
+        }
     }
 }
