739053198d0d409bc50f303c83963b6b8ab37438
Jereaaa <jere_calvet@hotmail.com>
2020-05-26 18:16:52 -0300

    Loop: ejercicio

--- part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -4,15 +4,35 @@ import java.util.Scanner;
 public class RepeatingBreakingAndRemembering {
 
     public static void main(String[] args) {
-        
-        // This exercise is worth five exercise points, and it is 
-        // gradually extended part by part.
-        
-        // If you want, you can send this exercise to the server
-        // when it's just partially done. In that case the server will complain about 
-        // the parts you haven't done, but you'll get points for the finished parts.
-        
-        Scanner scanner = new Scanner(System.in);
 
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int counter = 0;
+        int oddCounter = 0;
+        int evenCounter = 0;
+        System.out.println("Give numbers:");
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            
+            if (input == -1) {
+                break;
+            }
+            
+            sum += input;
+          
+            if (input % 2 == 0) {
+                evenCounter++;
+            } else {
+                oddCounter++;
+            }
+            
+            counter++;
+        }
+        System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + counter);
+        System.out.println("Average: " + (sum / (counter * 1.0)));
+        System.out.println("Even: " + evenCounter);
+        System.out.println("Odd: " + oddCounter);
     }
 }
