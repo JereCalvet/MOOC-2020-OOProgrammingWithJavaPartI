8018d4ac7fe0a0d42a3ff93c90ce8b3d17b0e993
Jereaaa <jere_calvet@hotmail.com>
2020-05-26 18:59:48 -0300

    Metodos

--- part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -2,8 +2,15 @@
 
 public class FromOneToParameter {
 
+    public static void printUntilNumber(int number){
+        int counter = 1;
+        while (counter <= number) {
+            System.out.println(counter);
+            counter++;
+        }
+    }
     public static void main(String[] args) {
-
+        printUntilNumber(2);
     }
 
 }
--- part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
+++ part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -1,9 +1,17 @@
 
-
 public class FromParameterToOne {
 
-    public static void main(String[] args) {
+    public static void printFromNumberToOne(int number) {
+        while (true) {
+            if (number == 0) {
+                break;
+            }
+            System.out.println(number);
+            number--;
+        }
+    }
 
+    public static void main(String[] args) {
 
     }
 
