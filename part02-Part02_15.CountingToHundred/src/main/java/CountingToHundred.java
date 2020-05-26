b2bb799669ae9082e0588c752ed9b9afa86e6d8f
Jereaaa <jere_calvet@hotmail.com>
2020-05-26 17:36:31 -0300

    Loops: ejercicio

--- part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -5,6 +5,10 @@ public class CountingToHundred {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int amount = Integer.valueOf(scanner.nextLine());
+        while (amount <= 100) {
+            System.out.println(amount);
+            amount++;
+        }
     }
 }
