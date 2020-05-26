57173768759f0a4e63666018774b87cd1421562b
Jereaaa <jere_calvet@hotmail.com>
2020-05-26 17:43:27 -0300

    Loops: ejercicio

--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -5,7 +5,13 @@ public class FromWhereToWhere {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
+        System.out.print("Where to? ");
+        int to = Integer.valueOf(scanner.nextLine());
+        System.out.print("Where from? ");
+        int from = Integer.valueOf(scanner.nextLine());
+        while (from <= to) {
+            System.out.println(from);
+            from++;
+        }
     }
 }
