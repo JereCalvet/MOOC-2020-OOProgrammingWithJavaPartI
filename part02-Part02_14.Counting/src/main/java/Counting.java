0a01dbb5dd13afe3e307f43a9d43d372ad9f2ad0
Jereaaa <jere_calvet@hotmail.com>
2020-05-26 17:32:24 -0300

    Loops: ejercicio

--- part02-Part02_14.Counting/src/main/java/Counting.java
+++ part02-Part02_14.Counting/src/main/java/Counting.java
@@ -5,6 +5,11 @@ public class Counting {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int amount = Integer.valueOf(scanner.nextLine());
+        int counter = 0;
+        while (amount >= counter) {
+            System.out.println(counter);
+            counter++;
+        }
     }
 }
