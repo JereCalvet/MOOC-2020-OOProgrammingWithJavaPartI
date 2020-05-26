b274b697b62c6e4bbc950c44ffe087c76ad4c320
Jereaaa <jere_calvet@hotmail.com>
2020-05-26 18:05:39 -0300

    Loop: ejercicio

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -5,6 +5,18 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.print("Give a number: ");
+        int num = Integer.valueOf(scanner.nextLine());
+        int counter = 1;
+        int sum = 1;
+        while (counter <= num) {
+            sum *= counter;
+            counter++;
+        }
+        if (num == 0) {
+            System.out.println("Factorial: 1");
+        } else {
+            System.out.println("Factorial: " + sum);
+        }
     }
 }
