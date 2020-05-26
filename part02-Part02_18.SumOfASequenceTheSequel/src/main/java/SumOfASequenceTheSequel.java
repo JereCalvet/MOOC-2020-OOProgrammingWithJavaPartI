02a72e365747384ad605aaa8b61696ca67e6712f
Jereaaa <jere_calvet@hotmail.com>
2020-05-26 17:54:17 -0300

    Loop: ejercicio

--- part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -5,6 +5,13 @@ public class SumOfASequenceTheSequel {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int firstNum = Integer.valueOf(scanner.nextLine());
+        int lastNum = Integer.valueOf(scanner.nextLine());
+        int sum = 0;
+        while (firstNum <= lastNum) {
+            sum += firstNum;
+            firstNum++;
+        }
+        System.out.println("The sum is " + sum);
     }
 }
