318e8c82f7ea8629effce959455d59b80d210d54
Jereaaa <jere_calvet@hotmail.com>
2020-05-27 19:19:09 -0300

    Metodos

--- part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -3,9 +3,17 @@ import java.util.Scanner;
 
 public class DivisibleByThree {
 
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        for (;beginning <= end; beginning++) {
+            if (beginning % 3 == 0) {
+                System.out.println(beginning);
+            }
+        }
+    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        divisibleByThreeInRange(2, 10);
     }
 
 }
