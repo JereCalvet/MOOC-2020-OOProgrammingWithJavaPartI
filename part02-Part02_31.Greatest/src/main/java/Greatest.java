cbe7722cef29cd09dffd640d8c931562a4d1d0de
Jereaaa <jere_calvet@hotmail.com>
2020-05-27 19:36:00 -0300

    Metodos

--- part02-Part02_31.Greatest/src/main/java/Greatest.java
+++ part02-Part02_31.Greatest/src/main/java/Greatest.java
@@ -1,10 +1,13 @@
 
+import java.util.Arrays;
+
+
 public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
-        //write some code here
-
-        return -1;
+        int[] ar = {number1, number2, number3};
+        Arrays.sort(ar);
+        return ar[2];
     }
 
     public static void main(String[] args) {
