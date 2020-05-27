2ebe0e3ee39965e35e61730e0c17dc6322e5bd18
Jereaaa <jere_calvet@hotmail.com>
2020-05-27 19:27:15 -0300

    Metodos

--- part02-Part02_28.Word/src/main/java/Word.java
+++ part02-Part02_28.Word/src/main/java/Word.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 
 public class Word {
 
+    public static String word() {
+        return "easy";
+    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
