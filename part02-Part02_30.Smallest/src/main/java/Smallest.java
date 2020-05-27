d28e7e9c6199cf1741c12800d415c342e53ecc7c
Jereaaa <jere_calvet@hotmail.com>
2020-05-27 19:31:38 -0300

    Metodos

--- part02-Part02_30.Smallest/src/main/java/Smallest.java
+++ part02-Part02_30.Smallest/src/main/java/Smallest.java
@@ -2,10 +2,8 @@
 public class Smallest {
 
     public static int smallest(int number1, int number2) {
-        // Write some code here
-        // don't print anything inside this method
-        // there must be a return in the end of the method
-        return -1;
+        
+        return number1 > number2 ? number2 : number1;
     }
 
     public static void main(String[] args) {
