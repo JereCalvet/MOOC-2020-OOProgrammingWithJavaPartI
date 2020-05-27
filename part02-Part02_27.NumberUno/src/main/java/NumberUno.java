a5071734a3004dc5d88233711dd88e00891a34d2
Jereaaa <jere_calvet@hotmail.com>
2020-05-27 19:23:09 -0300

    Metodos

--- part02-Part02_27.NumberUno/src/main/java/NumberUno.java
+++ part02-Part02_27.NumberUno/src/main/java/NumberUno.java
@@ -2,7 +2,10 @@
 import java.util.Scanner;
 
 public class NumberUno {
-
+    public static int numberUno() {
+        return 1;
+    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
