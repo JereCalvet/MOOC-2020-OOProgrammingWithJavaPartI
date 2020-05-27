3e14445d3a337b5c1e1f4c3c7699dda0e101cb53
Jereaaa <jere_calvet@hotmail.com>
2020-05-27 20:16:44 -0300

    Metodos

--- part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -2,19 +2,40 @@
 public class AdvancedAstrology {
 
     public static void printStars(int number) {
-        // part 1 of the exercise
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
     }
 
     public static void printSpaces(int number) {
-        // part 1 of the exercise
+        for (int i = 0; i < number; i++) {
+            System.out.print(" ");
+        }
     }
 
     public static void printTriangle(int size) {
-        // part 2 of the exercise
+        for (int i = 1; i <= size; i++) {
+            printSpaces(size - i);
+            printStars(i);
+        }
     }
 
+    public static void printStand(int xmasHeight) {
+        printSpaces(xmasHeight - 2);
+        printStars(3);
+        printSpaces(xmasHeight - 2);
+        printStars(3);
+    }
+    
     public static void christmasTree(int height) {
-        // part 3 of the exercise
+        int i = 1;
+        while (i <= height) {
+            printSpaces(height - i);
+            printStars(i + (i - 1));
+            i++;
+        }
+        printStand(height);
     }
 
     public static void main(String[] args) {
