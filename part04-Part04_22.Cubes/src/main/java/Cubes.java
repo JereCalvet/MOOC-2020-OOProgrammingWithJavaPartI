e8bdc984db5b3e450aa07fa5a14d0ff57c635b99
Jereaaa <jere_calvet@hotmail.com>
2020-06-07 19:43:25 -0300

    Files

--- part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,14 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            } else {
+                int num = Integer.valueOf(input);
+                System.out.println(num*num*num);
+            }
+        }
     }
 }
