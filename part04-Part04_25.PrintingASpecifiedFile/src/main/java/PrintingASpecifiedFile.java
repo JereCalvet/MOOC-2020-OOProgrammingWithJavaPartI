66f173dcafb4e2e1b321f211d34c253586d95ac2
Jereaaa <jere_calvet@hotmail.com>
2020-06-07 19:57:12 -0300

    Files: leer

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -5,7 +5,17 @@ import java.util.Scanner;
 public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
+        System.out.println("Which file should have its contents printed?");
         Scanner scanner = new Scanner(System.in);
-
+        String input = scanner.nextLine();
+        try {
+            Scanner reader = new Scanner(Paths.get(input));
+            while (reader.hasNextLine()) {
+                String row =  reader.nextLine();
+                System.out.println(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
