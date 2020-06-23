1aa61df52c9373f2ab36553b974bb731d0b3ab78
Jereaaa <jere_calvet@hotmail.com>
2020-06-23 18:47:12 -0300

    Procedural programming

--- part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
+++ part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 
 public class LiquidContainers {
 
+    //ty for forcing me to write bad code 
+    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int firstContainerLiquitAmount = 0;
+        int secondContainerLiquitAmount = 0;
 
         while (true) {
+            System.out.println("First: " + firstContainerLiquitAmount + "/100");
+            System.out.println("Second: " + secondContainerLiquitAmount + "/100");
             System.out.print("> ");
 
             String input = scan.nextLine();
@@ -15,6 +20,34 @@ public class LiquidContainers {
                 break;
             }
 
+            String[] parts = input.split(" ");
+
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+            switch (command) {
+                case "add":
+                    if (amount < 0) {
+                        break;
+                    }
+                    firstContainerLiquitAmount = firstContainerLiquitAmount + amount > 100 ? 100 : firstContainerLiquitAmount + amount;
+                    break;
+                case "move":
+                    if (amount < 0) {
+                        break;
+                    }
+                    int finalAmountToMove = firstContainerLiquitAmount < amount ? firstContainerLiquitAmount : amount;
+                    firstContainerLiquitAmount -= finalAmountToMove;
+                    secondContainerLiquitAmount = secondContainerLiquitAmount + finalAmountToMove > 100 ? 100 : secondContainerLiquitAmount + finalAmountToMove;
+                    break;
+                case "remove":
+                    if (amount < 0) {
+                        break;
+                    }
+                    int finalAmountToRemove = secondContainerLiquitAmount < amount ? secondContainerLiquitAmount : amount;
+                    secondContainerLiquitAmount -= finalAmountToRemove;
+                    break;
+            }
+            System.out.println();
         }
     }
 
