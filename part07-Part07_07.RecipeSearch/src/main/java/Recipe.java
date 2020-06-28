f7e489a5b0f091ac611bece7be880858079a8ea7
Jereaaa <jere_calvet@hotmail.com>
2020-06-27 22:27:02 -0300

    falta terminar

---
+++ part07-Part07_07.RecipeSearch/src/main/java/Recipe.java
@@ -0,0 +1,36 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Jere
+ */
+public class Recipe {
+    
+    private final String name;
+    private final int cookTime;
+    private List<String> ingredients;
+
+    public Recipe(String name, int cookTime) {
+        this.name = name;
+        this.cookTime = cookTime;
+        this.ingredients = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCookTime() {
+        return cookTime;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+    
+    public void addIngredient(String ingredient) {
+        ingredients.add(ingredient);
+    }
+}
---
+++ part07-Part07_07.RecipeSearch/src/main/java/UserInterface.java
@@ -0,0 +1,8 @@
+
+/**
+ *
+ * @author Jere
+ */
+public class UserInterface {
+    
+}
